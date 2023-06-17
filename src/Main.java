fd4fbda318d4d63d3da684daeb8aec8c229bfa50
machadinhos <pbmachado2002@gmail.com>
2023-06-17 19:36:34 +0100

    Added PieceInitializer.java to init all the Pieces

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import org.academiadecodigo.gamesweek.Pieces.PieceTypes.Bishop;
-import org.academiadecodigo.gamesweek.Pieces.Position;
-import org.academiadecodigo.gamesweek.Pieces.Team;
+import org.academiadecodigo.gamesweek.Pieces.PieceInitializer;
 import org.academiadecodigo.gamesweek.SimpleGFX.Board;
 
 
@@ -8,17 +6,20 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         Board.init();
 
-        Bishop bishopw = new Bishop(Team.WHITE, new Position(5, 1));
-        Bishop bishopb = new Bishop(Team.BLACK, new Position(7, 7));
+//        Bishop bishopw = new Bishop(Team.WHITE, new Position(5, 1));
+//        Bishop bishopb = new Bishop(Team.BLACK, new Position(7, 7));
+//
+//        Thread.sleep(500);
+//
+//        bishopb.getPosition().setNewPosition(2, 4);
+//        bishopb.getPicture().updateImagePosition();
+//
+//        Thread.sleep(500);
+//
+//        bishopb.getPosition().setNewPosition(7, 7);
+//        bishopb.getPicture().updateImagePosition();
 
-        Thread.sleep(500);
-
-        bishopb.getPosition().setNewPosition(2, 4);
-        bishopb.getPicture().updateImagePosition();
-
-        Thread.sleep(500);
-
-        bishopb.getPosition().setNewPosition(7, 7);
-        bishopb.getPicture().updateImagePosition();
+        PieceInitializer.initWhitePieces();
+        PieceInitializer.initBlackPieces();
     }
 }
