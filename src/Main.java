191e514b6adaa3c8a7228bcc704abd1da0fa5433
machadinhos <pbmachado2002@gmail.com>
2023-06-17 13:17:59 +0100

    Create Field.java constructor

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
+import org.academiadecodigo.gamesweek.SimpleGFX.Field;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(10,10,1900,1040);
-        rectangle.draw();
+        new Field();
     }
 }
--- src/org/academiadecodigo/gamesweek/SimpleGFX/Field.java
+++ src/org/academiadecodigo/gamesweek/SimpleGFX/Field.java
@@ -1,13 +1,38 @@
 package org.academiadecodigo.gamesweek.SimpleGFX;
 
+import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
 public class Field {
+     private static final int SQUARESIZE = 70;
+     private static final int PADDING = 10;
+     private static final Rectangle[][] boardSquares = new Rectangle[8][8];
 
      public Field(){
+          int xPosition = SQUARESIZE + PADDING;
+          int yPosition = SQUARESIZE + PADDING;
+          Color color = Color.WHITE;
+
+          for (int row = 0; row < boardSquares.length; row++) {
+               for (int square = 0; square < boardSquares[row].length; square++) {
+                    boardSquares[row][square] = new Rectangle(xPosition, yPosition, SQUARESIZE, SQUARESIZE);
+                    boardSquares[row][square].setColor(color);
+                    boardSquares[row][square].fill();
+
+                    if (color.equals(Color.WHITE)) {
+                         color = Color.BLACK;
+                    } else {
+                         color = Color.WHITE;
+                    }
+                    xPosition += SQUARESIZE;
+               }
+               xPosition = SQUARESIZE + PADDING;
+               yPosition += SQUARESIZE;
+          }
+
      }
 
-     public Rectangle createField(){
-
+     public Rectangle createField() throws UnsupportedOperationException {
+          throw new UnsupportedOperationException();
      }
 }
